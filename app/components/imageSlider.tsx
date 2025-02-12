b275dcf435cf58054b4aa8fb31350f7b30f3c4b4
Elxan <elxan.offical.07@gmail.com>
2025-02-13 01:06:06 +0400

    Third Party Packages

--- app/components/imageSlider.tsx
+++ app/components/imageSlider.tsx
@@ -5,7 +5,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
 
-export default function ClientRoutePage() {
+export default function ImageSlider() {
   const settings = {
     dots: true,
   };
--- app/server-route/page.tsx
+++ app/server-route/page.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
 import { ServerSideFunction } from '../utils/server-utils'
+import ImageSlider from '../components/imageSlider'
 
 export default function ServerRoutePage() {
   console.log('server route rendered')
   const result = ServerSideFunction()
   return (
-    <div>
+    <div> 
       <h1>ServerRoutePage</h1>
       <p>{result}</p>
+      <ImageSlider/>
     </div>
   )
 }
